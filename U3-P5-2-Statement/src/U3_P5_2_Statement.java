6009ec5504786aba41e08ca69c59fd728010298c
Rafa Puig <rafapuig.profe@gmail.com>
2022-10-12 20:09:46 +0200

    ddd

--- U3-P5-2-Statement/src/U3_P5_2_Statement.java
+++ U3-P5-2-Statement/src/U3_P5_2_Statement.java
@@ -1,10 +1,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class U3_P5_2_Statement {
 
     public static void main(String... args){
-        Connection c = DriverManager.getConnection("");
+        try {
+            Connection c = DriverManager.getConnection("");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
---
+++ U3-P5-3-Statement/src/U3_P5_3_Statement.java
@@ -0,0 +1,5 @@
+public class U3_P5_3_Statement {
+    public static void main(String... args){
+        System.out.println("Hola mundo!!!");
+    }
+}
