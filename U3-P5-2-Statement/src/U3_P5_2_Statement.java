d309ee838d0156c9e725f33870960aa00bf0b9ad
Rafa Puig <rafapuig.profe@gmail.com>
2022-10-12 20:04:39 +0200

    bbb

--- U3-P5-2-Statement/src/U3_P5_2_Statement.java
+++ U3-P5-2-Statement/src/U3_P5_2_Statement.java
@@ -1,6 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class U3_P5_2_Statement {
 
     public static void main(String... args){
+        Connection c = DriverManager.getConnection("");
 
     }
 }
